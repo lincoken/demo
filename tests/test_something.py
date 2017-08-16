1c2521c7a14863f00845aae846a00c915188ec7b
Kennedy Lincoln <kennedylincoln@icloud.com>
2017-08-16 19:16:37 +0100

    adding new tests to improve coverage and ractice writing unittests

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,6 +1,18 @@
 import pytest
 
-def test_square():
+def test_square_2():
     from demo.demo import square
 
-    assert 5 == square(2)
+    assert 4 == square(2)
+
+    
+def test_square_negitive():
+    from demo.demo import square
+
+    assert 4 == square(-2)
+
+    
+def test_square_decimal():
+    from demo.demo import square
+
+    assert 6.25 == square(2.5)
